8f37b9f4346b74a8b2e403fc5e3b901a3047f05d
syong88 <71371743+syong88@users.noreply.github.com>
2021-07-28 14:44:54 +0900

    Add files via upload

---
+++ precache-manifest.bf688549d962a61b7d0a57a56c64719c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0faadd2e6001f1085d81",
+    "url": "/css/app.42a96f88.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0c7a7ad8c9f49b8579ee93186898053c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0faadd2e6001f1085d81",
+    "url": "/js/app.b21d43eb.js"
+  },
+  {
+    "revision": "da1bb54520aa2c07953d",
+    "url": "/js/chunk-vendors.77185ccf.js"
+  },
+  {
+    "revision": "47ca0b75fa643d43a739580f6d60f77d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
